258b5ea0b0b7e0b6445aab145a4ec3fd8e619de3
jim-conner <jamesdavidconner@gmail.com>
2021-08-28 09:43:42 -0500

    removed skills and services from Spy

--- SpyDuhApiProject2/DataAccess/SpyRepository.cs
+++ SpyDuhApiProject2/DataAccess/SpyRepository.cs
@@ -15,32 +15,24 @@ namespace SpyDuhApiProject2.DataAccess
                 Alias = "Harry",
                 Id = Guid.Parse("e7c4af1e-c8d4-4998-a611-0b31cc62d312"),
                 AboutMe = "I'm super nosy so I became a spy.",
-                Skills = new List<string>{ "Stealth", "Investigation", "camouflage" },
-                Services = new List<string>{"Breaking in to read people's diaries", "Infiltrating an organization"}
             },
              new Spy
             {
                 Alias = "Larry",
                 Id = Guid.Parse("11692ac4-9d81-4be5-8fd3-5154b1579a94"),
                 AboutMe = "I became a spy to take down evil corporations.",
-                Skills = new List<string> { "Hacking", "Investigation", "Impersonation" },
-                Services = new List<string> {"Breaking in to read people's diaries", "Hacking into a corporation's sensitive data"}
             },
               new Spy
             {
                 Alias = "Barry",
                 Id = Guid.Parse("0a0498c4-6d99-4de8-b687-127a0b89bb2a"),
                 AboutMe = "Becoming a spy sounded cool.",
-                Skills = new List<string> { "Stealth", "Impersonation", "camouflage" },
-                Services = new List<string> {"Impersonating high-profile people", "Breaking into safes"}
             },
                new Spy
             {
                 Alias = "Carry",
                 Id = Guid.Parse("14d2d829-a609-4fe4-82ad-5dab2444e274"),
                 AboutMe = "It's fun to do hood rat stuff with your friends.",
-                Skills = new List<string> { "Stealth", "Armed Robbery", "GTA" },
-                Services = new List<string> {"Burning your grandmother's cookies", "Infiltrating a book club"}
             },
         };
 
